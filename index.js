7c6a8cd02353a7346c4fb75ae03fec84cb3d099a
jukebox101 <leonora.squires@gmail.com>
2020-06-23 13:11:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arr, eventName){
+    let newArr = [];
+    for(let i=0; i < arr.length; i++){
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return newArr;
+}
+
+function countDown(count){
+    while (count >= 0){
+        console.log(count)
+        count--;
+    }
+}
